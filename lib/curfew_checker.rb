5b7b5b34c99f854daaacd82608b03235de3eae4a
Sonali Sharma <sonalisharma58521@gmail.com>
2018-07-02 18:15:15 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,6 +1,18 @@
 def simple_curfew_checker(time)
   # code goes here
-end
+  curfew= 11
+  if time > 11
+    puts "You're in trouble! Better get home quick!"
+  elsif time ==11
+    puts "Your barely made it!"
+  else time<11
+    puts "Good job for getting here before curfew! You had only #{
+  end
+end 
+simple_curfew_checker(10)
+
+
+
 
 def curfew_checker(time)
   # code goes here
