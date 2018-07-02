755d79e50213e6a1ee199b692fa096375ce18a85
Sonali Sharma <sonalisharma58521@gmail.com>
2018-07-02 18:17:19 +0000

    Done.

---
+++ curfew_checker.rb
@@ -0,0 +1,18 @@
+if 4 < 5 
+ # puts "four is less than five."
+
+
+elsif 7 > 5 
+  puts " sevan is greater than five"
+else
+  puts "seven is less than five"
+end
+
+
+ if 4 > 5 
+   puts "10 is greater than five"
+  elsif 10 == 5
+   puts "ten equals five"
+  else
+    puts "four  isn't greater than five and ten isnt equal to 5"
+end
